18cd6b720be0d7e3f8326d32b392590bae719e70
richarddubay <richardtdubayjr@gmail.com>
2019-04-13 21:44:01 -0400

    some work on the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -48,9 +48,25 @@ class Layout extends React.Component {
           <Header />
         </header>
         <main className="main">{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with{" "}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer class="footer">
+          <p>
+            © {new Date().getFullYear()}, Built with{" "}
+            <a href="https://www.gatsbyjs.org">Gatsby</a> and ❤️
+          </p>
+          <ul>
+            <li>
+              <a href="https://twitter.com/richarddubay">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/richarddubay/">Instagram</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/richarddubay">Facebook</a>
+            </li>
+            <li>
+              <a href="https://github.com/richarddubay">Github</a>
+            </li>
+          </ul>
         </footer>
       </div>
     )

9a11136873ac412a4aafeb244ec7efbc63ac2f1e
richarddubay <richardtdubayjr@gmail.com>
2019-04-14 20:17:37 -0400

    removed unnecessary blog-post styles file

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import moment from "moment"
 
-import "./blog-post.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 

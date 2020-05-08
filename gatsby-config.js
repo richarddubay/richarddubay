d38ea2961ca5bea2fdb422e2e2002c4d17cb687a
richarddubay <richardtdubayjr@gmail.com>
2020-05-08 08:33:31 -0400

    Made the posts images stretch across the whole width of the grid section if possible

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,65 +1,65 @@
 module.exports = {
   siteMetadata: {
-    title: "Richard Dubay",
-    author: "Richard Dubay",
+    title: 'Richard Dubay',
+    author: 'Richard Dubay',
     description:
-      "A dead man made alive through Jesus. Husband. Dad. Developer at NewSpring Church.",
-    image: "share-image.png",
-    siteUrl: "https://www.richarddubay.com/",
+      'A dead man made alive through Jesus. Husband. Dad. Developer at NewSpring Church.',
+    image: 'share-image.png',
+    siteUrl: 'https://www.richarddubay.com/',
     social: {
       facebook: {
-        shareImage: "/facebook-share-image.png",
+        shareImage: '/facebook-share-image.png',
       },
       twitter: {
-        profileName: "@richarddubay",
-        shareImage: "/twitter-share-image.png",
+        profileName: '@richarddubay',
+        shareImage: '/twitter-share-image.png',
       },
     },
   },
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "blog",
+        name: 'blog',
         path: `${__dirname}/content/blog`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "assets",
+        name: 'assets',
         path: `${__dirname}/content/assets`,
       },
     },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         pedantic: false,
         plugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 1200,
+              maxWidth: 1600,
             },
           },
           {
-            resolve: "gatsby-remark-responsive-iframe",
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: "margin-bottom: 1.0725rem",
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-125092221-1",
+        trackingId: 'UA-125092221-1',
       },
     },
     {
@@ -80,15 +80,15 @@ module.exports = {
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
+              return allMarkdownRemark.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
             },
             query: `
               {
@@ -109,44 +109,44 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
+            output: '/rss.xml',
             title: "Rich's RSS Feed",
           },
         ],
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Rich Dubay",
-        short_name: "Rich Dubay",
-        start_url: "/",
-        background_color: "#ffffff",
-        theme_color: "#663399",
-        display: "standalone",
-        icon: "content/assets/gray-icon.png", // This path is relative to the root of the site.
+        name: 'Rich Dubay',
+        short_name: 'Rich Dubay',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#663399',
+        display: 'standalone',
+        icon: 'content/assets/gray-icon.png', // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-prefetch-google-fonts",
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
-            family: "Merriweather",
-            variants: ["300", "300i", "400", "400i", "700"],
+            family: 'Merriweather',
+            variants: ['300', '300i', '400', '400i', '700'],
           },
           {
-            family: "Roboto Condensed",
-            variants: ["400", "700"],
+            family: 'Roboto Condensed',
+            variants: ['400', '700'],
           },
           {
-            family: "Inconsolata",
-            variants: ["400"],
+            family: 'Inconsolata',
+            variants: ['400'],
           },
         ],
       },
     },
   ],
-}
+};
